21e3c7d37ec7330b937125a18398a0cf6147ff6d
Mohit Soni <anupam.jain@ranosys.com>
2019-01-11 15:59:21 +0530

    intial commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_item, second_item):
+    return first_item + second_item
+
+def sub(first_item, second_item):
+    return first_item - second_item
+
+def mul(first_item, second_item):
+    return first_item * second_item
+
+def devide(first_item, second_item):
+    return first_item / second_item
